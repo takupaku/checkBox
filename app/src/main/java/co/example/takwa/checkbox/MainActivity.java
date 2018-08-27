ab782242bc2503587e37ca27bc037b0fef811074
taku <takupaku@yahoo.com>
2018-08-28 01:06:29 +0600

    do quick

---
+++ app/src/main/java/co/example/takwa/checkbox/MainActivity.java
@@ -0,0 +1,59 @@
+package co.example.takwa.checkbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    CheckBox checkBox1,checkBox2,checkBox3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView= findViewById(R.id.result);
+        checkBox1= findViewById(R.id.riceId);
+                checkBox2= findViewById(R.id.meatId);
+                        checkBox3= findViewById(R.id.chickenId);
+    }
+
+    public void checkBox(View view) {
+        StringBuilder stringBuilder= new StringBuilder("You have selected \n");
+        int checker=0;
+
+                if(checkBox1.isChecked()){
+            String s= checkBox1.getText().toString();
+            stringBuilder.append(s+"\n");
+
+                }
+
+
+        if(checkBox2.isChecked()){
+            String s= checkBox2.getText().toString();
+            stringBuilder.append(s+"\n");
+        }
+
+        if(checkBox3.isChecked()){
+            String s= checkBox3.getText().toString();
+            stringBuilder.append(s+"\n");
+        }
+        if(checkBox1.isChecked() ||checkBox2.isChecked()|| checkBox3.isChecked()){
+                    checker=1;
+        }
+        else{
+                    checker=0;
+        }
+        if(checker==0){
+                    textView.setText("you have selected nothing");
+        }
+        else{
+            textView.setText(stringBuilder);
+        }
+
+
+    }
+}
